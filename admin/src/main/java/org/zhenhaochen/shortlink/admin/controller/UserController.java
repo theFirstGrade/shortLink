caab2da54e64b795a8e19375f3d5a82b6c4d814f
theFirstGrade <chen.zhenh@northeastern.edu>
2024-05-20 16:35:31 -0400

    feature: develop shortlink admin system

--- admin/src/main/java/org/zhenhaochen/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/org/zhenhaochen/shortlink/admin/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.zhenhaochen.shortlink.admin.common.convention.result.Result;
 import org.zhenhaochen.shortlink.admin.common.convention.result.Results;
-import org.zhenhaochen.shortlink.admin.common.enums.UserErrorCodeEnum;
 import org.zhenhaochen.shortlink.admin.dto.resp.UserRespDTO;
 import org.zhenhaochen.shortlink.admin.service.UserService;
 
@@ -24,11 +23,6 @@ public class UserController {
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
-        UserRespDTO result = userService.getUserByUsername(username);
-        if (result == null) {
-            return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).setMessage(UserErrorCodeEnum.USER_NULL.message());
-        } else {
-            return Results.success(result);
-        }
+        return Results.success(userService.getUserByUsername(username));
     }
 }

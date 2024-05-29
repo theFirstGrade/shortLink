89845c0b943dd44f711e21d094e13ee694ab4889
theFirstGrade <chen.zhenh@northeastern.edu>
2024-05-29 19:04:50 -0400

    feature: MySQL paging plugin

---
+++ admin/src/main/java/org/zhenhaochen/shortlink/admin/config/DataBaseConfiguration.java
@@ -0,0 +1,21 @@
+package org.zhenhaochen.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * paging plugin
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}

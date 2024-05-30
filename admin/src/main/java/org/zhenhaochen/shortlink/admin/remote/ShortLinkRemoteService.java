df2cccd7c627db1c7779efed2d145342dd0c3de5
theFirstGrade <chen.zhenh@northeastern.edu>
2024-05-30 16:25:13 -0400

    optimize: optimize package

--- admin/src/main/java/org/zhenhaochen/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/org/zhenhaochen/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -1,4 +1,4 @@
-package org.zhenhaochen.shortlink.admin.remote.dto;
+package org.zhenhaochen.shortlink.admin.remote;
 
 import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson2.JSON;
@@ -8,9 +8,11 @@ import org.zhenhaochen.shortlink.admin.common.convention.result.Result;
 import org.zhenhaochen.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import org.zhenhaochen.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
 import org.zhenhaochen.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import org.zhenhaochen.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import org.zhenhaochen.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -36,6 +38,18 @@ public interface ShortLinkRemoteService {
         requestMap.put("current", requestParam.getCurrent());
         requestMap.put("size", requestParam.getSize());
         String resultPageStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/page", requestMap);
+        return JSON.parseObject(resultPageStr, new TypeReference<>() {
+        });
+    }
+
+    /**
+     * query short link count in a group
+     * @return short link count Object
+     */
+    default Result<List<ShortLinkGroupCountQueryRespDTO>> listGroupShortLinkCount(List<String> requestParam) {
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("requestParam", requestParam);
+        String resultPageStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/count", requestMap);
         return JSON.parseObject(resultPageStr, new TypeReference<>() {
         });
     }

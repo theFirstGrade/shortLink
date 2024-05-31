cc76074266b27b3ced63f2ed12de09d89e3ec21f
theFirstGrade <chen.zhenh@northeastern.edu>
2024-05-30 22:03:40 -0400

    feature: modify 'PutMapping' to 'PostMapping'

--- admin/src/main/java/org/zhenhaochen/shortlink/admin/controller/ShortLinkController.java
+++ admin/src/main/java/org/zhenhaochen/shortlink/admin/controller/ShortLinkController.java
@@ -34,7 +34,7 @@ public class ShortLinkController {
     /**
      * update short link
      */
-    @PutMapping("/api/short-link/admin/v1/update")
+    @PostMapping("/api/short-link/admin/v1/update")
     public Result<Void> updateShortLink(@RequestBody ShortLinkUpdateReqDTO requestParam) {
         shortLinkRemoteService.updateShortLink(requestParam);
         return Results.success();

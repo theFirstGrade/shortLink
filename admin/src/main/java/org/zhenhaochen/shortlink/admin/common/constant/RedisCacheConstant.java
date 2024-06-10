5a8b5c6c77b4925a0a5b0b2edae84bfa0b083fbf
theFirstGrade <chen.zhenh@northeastern.edu>
2024-06-10 18:24:41 -0400

    optimize: the size limit of single user's group is 20

--- admin/src/main/java/org/zhenhaochen/shortlink/admin/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/org/zhenhaochen/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -2,5 +2,13 @@ package org.zhenhaochen.shortlink.admin.common.constant;
 
 public class RedisCacheConstant {
 
+    /**
+     * user registration distributed lock prefix
+     */
     public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+
+    /**
+     * group creation distributed lock prefix
+     */
+    public static final String LOCK_GROUP_CREATE_KEY = "short-link:lock_group-create:%s";
 }
--- admin/src/main/java/org/zhenhaochen/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/org/zhenhaochen/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,10 +1,15 @@
 package org.zhenhaochen.shortlink.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.zhenhaochen.shortlink.admin.common.biz.user.UserContext;
 import org.zhenhaochen.shortlink.admin.common.convention.exception.ClientException;
@@ -23,8 +28,16 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+import static org.zhenhaochen.shortlink.admin.common.constant.RedisCacheConstant.LOCK_GROUP_CREATE_KEY;
+
 @Service
+@RequiredArgsConstructor
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+
+    private final RedissonClient redissonClient;
+
+    @Value("${short-link.group.max-num}")
+    private Integer groupMaxNum;
 
     /**
      * rebuild SpringCloud later
@@ -39,17 +52,30 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     @Override
     public void saveGroup(String username, String groupName) {
-        String gid;
-        do {
-            gid = RandomGenerator.generateRandom();
-        } while (hasGid(username, gid));
-        GroupDO groupDo = GroupDO.builder()
-                .gid(gid)
-                .sortOrder(0)
-                .username(username)
-                .name(groupName)
-                .build();
-        baseMapper.insert(groupDo);
+        RLock lock = redissonClient.getLock(String.format(LOCK_GROUP_CREATE_KEY, username));
+        lock.lock();
+        try {
+            LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                    .eq(GroupDO::getUsername, username)
+                    .eq(GroupDO::getDelFlag, 0);
+            List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+            if (CollUtil.isNotEmpty(groupDOList) && groupDOList.size() == groupMaxNum) {
+                throw new ClientException(String.format("exceed maximum group：%d", groupMaxNum));
+            }
+            String gid;
+            do {
+                gid = RandomGenerator.generateRandom();
+            } while (hasGid(username, gid));
+            GroupDO groupDO = GroupDO.builder()
+                    .gid(gid)
+                    .sortOrder(0)
+                    .username(username)
+                    .name(groupName)
+                    .build();
+            baseMapper.insert(groupDO);
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override

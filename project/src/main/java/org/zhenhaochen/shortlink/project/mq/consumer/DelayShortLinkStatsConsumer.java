7e7a8fdc872bff2bb132c37a814055dc8a1a2f8e
theFirstGrade <chen.zhenh@northeastern.edu>
2024-06-18 18:00:33 -0400

    optimize: delete delayed queue

--- project/src/main/java/org/zhenhaochen/shortlink/project/mq/consumer/DelayShortLinkStatsConsumer.java
+++ project/src/main/java/org/zhenhaochen/shortlink/project/mq/consumer/DelayShortLinkStatsConsumer.java
@@ -70,6 +70,6 @@ public class DelayShortLinkStatsConsumer implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        onMessage();
+//        onMessage();
     }
 }
